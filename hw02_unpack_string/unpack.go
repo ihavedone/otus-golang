0c7850240279bf60e3f9340f98478689c65a0d63
Artem Merets <a.merets@tinkoff.ru>
2023-06-12 13:27:56 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,64 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+var ErrUnexpectedSlash = errors.New("unexpected slash")
+var ErrUnexpectedDigit = errors.New("unexpected digit")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isDigit(current string) bool {
+	_, err := strconv.ParseInt(current, 10, 64)
+	if err == nil {
+		return true
+	}
+	return false
+}
+
+func extractSymbolAndGetTail(input string) (string, string, error) {
+	if len(input) == 0 {
+		return "", "", nil
+	}
+
+	runes, offset := []rune(input), 1
+	currentSymbol := string(runes[:offset])
+	if isDigit(currentSymbol) {
+		return "", "", ErrUnexpectedDigit
+	}
+
+	if currentSymbol == "\\" {
+		offset = 2
+		currentSymbol = string(runes[offset-1 : offset])
+		if !isDigit(currentSymbol) && currentSymbol != "\\" {
+			return "", "", ErrUnexpectedSlash
+		}
+	}
+	if offset < len(runes) {
+		nextRune := string(runes[offset : offset+1])
+		if isDigit(nextRune) {
+			parsedInt, _ := strconv.ParseInt(nextRune, 10, 64)
+			currentSymbol = strings.Repeat(currentSymbol, int(parsedInt))
+			offset += 1
+		}
+	}
+	return currentSymbol, string(runes[offset:]), nil
+}
+
+func Unpack(input string) (string, error) {
+	var inputTail = input
+	var result strings.Builder
+	var err error
+	for {
+		var currentSymbol string
+		currentSymbol, inputTail, err = extractSymbolAndGetTail(inputTail)
+		if err != nil {
+			return "", err
+		}
+		result.WriteString(currentSymbol)
+		if len(inputTail) <= 0 {
+			break
+		}
+	}
+	return result.String(), nil
 }
