d09c2f176a72e159663e8c4b6d7d1122f771112b
Artem Merets <a.merets@tinkoff.ru>
2023-06-29 00:29:57 +0300

    HW03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,75 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+const EXPECTED_SLICE_SIZE = 10
+
+var blackListWords = []string{"", "-"}
+var separatorRegexp = regexp.MustCompile(`[-]?[\s,.!?'":;@#$%^&*(){}<>\[\]_]+[-]?`)
+
+type Word struct {
+	value   string
+	counter int
+}
+type Words []Word
+
+func (wordsPtr *Words) add(text string) {
+	for _, v := range blackListWords {
+		if text == v {
+			return
+		}
+	}
+	*wordsPtr = append(*wordsPtr, Word{value: text, counter: 1})
+}
+
+func (wordsPtr *Words) Calc(text string) {
+	for indx := range *wordsPtr {
+		if (*wordsPtr)[indx].value == text {
+			(*wordsPtr)[indx].counter++
+			return
+		}
+	}
+	wordsPtr.add(text)
+}
+
+func (words Words) Sort() Words {
+	sort.Slice(words, func(i, j int) bool {
+		if (words)[i].counter == (words)[j].counter {
+			return (words)[i].value < (words)[j].value
+		}
+		return (words)[i].counter > (words)[j].counter
+	})
+	return words
+}
+
+func (words Words) Head(n int) Words {
+	var output Words
+	for _, word := range words {
+		output = append(output, word)
+		if len(output) >= n {
+			break
+		}
+	}
+	return output
+}
+
+func (words Words) ToStrings() []string {
+	var output []string
+	for _, word := range words {
+		output = append(output, word.value)
+	}
+	return output
+}
+
+func Top10(text string) []string {
+	tokens := separatorRegexp.Split(text, -1)
+	var words Words
+	for _, token := range tokens {
+		words.Calc(strings.ToLower(token))
+	}
+	return words.Sort().Head(EXPECTED_SLICE_SIZE).ToStrings()
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -6,9 +6,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Change to true if needed.
-var taskWithAsteriskIsCompleted = false
-
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
 	ступеньки собственным затылком:  бум-бум-бум.  Другого  способа
@@ -49,34 +46,36 @@ func TestTop10(t *testing.T) {
 	})
 
 	t.Run("positive test", func(t *testing.T) {
-		if taskWithAsteriskIsCompleted {
-			expected := []string{
-				"а",         // 8
-				"он",        // 8
-				"и",         // 6
-				"ты",        // 5
-				"что",       // 5
-				"в",         // 4
-				"его",       // 4
-				"если",      // 4
-				"кристофер", // 4
-				"не",        // 4
-			}
-			require.Equal(t, expected, Top10(text))
-		} else {
-			expected := []string{
-				"он",        // 8
-				"а",         // 6
-				"и",         // 6
-				"ты",        // 5
-				"что",       // 5
-				"-",         // 4
-				"Кристофер", // 4
-				"если",      // 4
-				"не",        // 4
-				"то",        // 4
-			}
-			require.Equal(t, expected, Top10(text))
+		expected := []string{
+			"а",         // 8
+			"он",        // 8
+			"и",         // 6
+			"ты",        // 5
+			"что",       // 5
+			"в",         // 4
+			"его",       // 4
+			"если",      // 4
+			"кристофер", // 4
+			"не",        // 4
 		}
+		require.Equal(t, expected, Top10(text))
 	})
+}
+
+func TestSmiles(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []string
+	}{
+		{input: ":} Ты шо))) тоже мне шо >:-]", expected: []string{"шо", "мне", "тоже", "ты"}},
+		{input: "Рак <3 сказал раку >_> \"Рак!\", X) :DDD", expected: []string{"рак", "3", "ddd", "x", "раку", "сказал"}},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result := Top10(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
 }
